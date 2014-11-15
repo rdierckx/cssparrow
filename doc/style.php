875cc46783fdbb890f67efb454d923e2bf1f2e1f
Romain <romaindierckx@gmail.com>
2014-11-15 23:38:08 +0100

    change lib document name to src

--- doc/style.php
+++ doc/style.php
@@ -1,4 +1,4 @@
-<?php require "lib/markdown.php"; ?>
+<?php require "src/markdown.php"; ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
